eef64e695bcbbd40731c09abe4a0e2ea0b86e706
Theo Vora <theo.vora@gmail.com>
2023-11-28 12:55:05 -0800

    add compatibilities for Goat, Rat, Ox, Tiger

--- Tests/ChineseZodiacCompatibilityTests/CompatibilitiesTests.swift
+++ Tests/ChineseZodiacCompatibilityTests/CompatibilitiesTests.swift
@@ -3,9 +3,39 @@ import ChineseZodiacCompatibility
 
 final class CompatibilitiesTests: XCTestCase {
     func test_horseIsCompatibleWith_Goat_Dog_Tiger() {
-        let horseCompatibilities = Compatibilities.of(animal: .Horse)
-        XCTAssertTrue(horseCompatibilities.contains(.Dog))
-        XCTAssertTrue(horseCompatibilities.contains(.Goat))
-        XCTAssertTrue(horseCompatibilities.contains(.Tiger))
+        let sut = Compatibilities.of(animal: .Horse)
+        XCTAssertTrue(sut.contains(.Dog))
+        XCTAssertTrue(sut.contains(.Goat))
+        XCTAssertTrue(sut.contains(.Tiger))
+    }
+    
+    func test_goatIsCompatibleWith_Goat_Rabbit_Horse_Pig() throws {
+        let sut = Compatibilities.of(animal: .Goat)
+        XCTAssertTrue(sut.contains(.Goat))
+        XCTAssertTrue(sut.contains(.Rabbit))
+        XCTAssertTrue(sut.contains(.Horse))
+        XCTAssertTrue(sut.contains(.Pig))
+    }
+    
+    func test_ratIsCompatibleWith_Rat_Ox_Dragon_Monkey() throws {
+        let sut = Compatibilities.of(animal: .Rat)
+        XCTAssertTrue(sut.contains(.Rat))
+        XCTAssertTrue(sut.contains(.Ox))
+        XCTAssertTrue(sut.contains(.Dragon))
+        XCTAssertTrue(sut.contains(.Monkey))
+    }
+    
+    func test_whenAnimalIsOx_compatibilitiesContain_Rat_Rooster_Snake() throws {
+        let sut = Compatibilities.of(animal: .Ox)
+        XCTAssertTrue(sut.contains(.Rat))
+        XCTAssertTrue(sut.contains(.Rooster))
+        XCTAssertTrue(sut.contains(.Snake))
+    }
+    
+    func test_whenAnimalIsTiger_compatibilitiesContain_Horse_Dog_Pig() throws {
+        let sut = Compatibilities.of(animal: .Tiger)
+        XCTAssertTrue(sut.contains(.Horse))
+        XCTAssertTrue(sut.contains(.Dog))
+        XCTAssertTrue(sut.contains(.Pig))
     }
 }
