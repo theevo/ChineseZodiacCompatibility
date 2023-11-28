7474cb89fc5317a0290c7bdfbfd150115e171122
Theo Vora <theo.vora@gmail.com>
2023-11-28 12:47:17 -0800

    establish Animal enum since there are only 12

--- Sources/ChineseZodiacCompatibility/Compatibilities.swift
+++ Sources/ChineseZodiacCompatibility/Compatibilities.swift
@@ -8,7 +8,57 @@
 import Foundation
 
 public struct Compatibilities {
-    public static func of(animal: String) -> [String] {
-        return ["Dog"]
+    public static func of(animal: Animal) -> [Animal] {
+        switch animal {
+            
+            // correct
+        case .Rat:
+            return [.Rat, .Ox, .Dragon, .Monkey]
+        case .Ox:
+            return [.Rat, .Rooster, .Snake]
+        case .Tiger:
+            return [.Dog, .Horse, .Pig]
+            
+            // NOT CORRECT
+        case .Rabbit:
+            return [.Dog, .Goat, .Tiger]
+        case .Dragon:
+            return [.Goat, .Rabbit, .Horse, .Pig]
+        case .Snake:
+            return [.Dog, .Goat, .Tiger]
+        case .Goat:
+            return [.Goat, .Rabbit, .Horse, .Pig]
+            
+            // correct
+        case .Horse:
+            return [.Dog, .Goat, .Tiger]
+        case .Monkey:
+            return [.Goat, .Rabbit, .Horse, .Pig]
+            
+            // NOT CORRECT
+        case .Rooster:
+            return [.Dog, .Goat, .Tiger]
+        case .Dog:
+            return [.Goat, .Rabbit, .Horse, .Pig]
+        case .Pig:
+            return [.Dog, .Goat, .Tiger]
+        }
+    }
+}
+
+extension Compatibilities {
+    public enum Animal: String {
+        case Rat
+        case Ox
+        case Tiger
+        case Rabbit
+        case Dragon
+        case Snake
+        case Horse
+        case Goat
+        case Monkey
+        case Rooster
+        case Dog
+        case Pig
     }
 }
--- Tests/ChineseZodiacCompatibilityTests/CompatibilitiesTests.swift
+++ Tests/ChineseZodiacCompatibilityTests/CompatibilitiesTests.swift
@@ -2,8 +2,10 @@ import XCTest
 import ChineseZodiacCompatibility
 
 final class CompatibilitiesTests: XCTestCase {
-    func test_horseIsCompatibleWith_Sheep_Dog_Tiger() {
-        let horseCompatibilities = Compatibilities.of(animal: "Horse")
-        XCTAssertTrue(horseCompatibilities.contains("Dog"))
+    func test_horseIsCompatibleWith_Goat_Dog_Tiger() {
+        let horseCompatibilities = Compatibilities.of(animal: .Horse)
+        XCTAssertTrue(horseCompatibilities.contains(.Dog))
+        XCTAssertTrue(horseCompatibilities.contains(.Goat))
+        XCTAssertTrue(horseCompatibilities.contains(.Tiger))
     }
 }
